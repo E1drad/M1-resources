b7d59d87d2ab7de15abff37848ed799c84604fb1
E1drad <charles.eric44@gmail.com>
2016-10-04 17:08:18 +0200

    Update WebSem TP1 - Literals and Datatypes

--- Web semantique/JenaSandBox/src/tp1/paf.java
+++ Web semantique/JenaSandBox/src/tp1/paf.java
@@ -16,6 +16,7 @@ import org.apache.jena.rdf.model.SimpleSelector;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.util.FileManager;
+import org.apache.jena.vocabulary.RDFS;
 import org.apache.jena.vocabulary.VCARD;
 
 
@@ -26,49 +27,31 @@ public class paf {
 	static final String inputFileName = "src/tp1/vc-db-1.rdf";
     
 	public static void main (String args[]) {
-
+		
 		Model model = ModelFactory.createDefaultModel();
-		 // use the class loader to find the input file
-        InputStream in = FileManager.get().open( inputFileName );
-        if (in == null) {
-            throw new IllegalArgumentException( "File: " + inputFileName + " not found");
-        }
-        
-        // read the RDF/XML file
-        model.read(new InputStreamReader(in), "");
-        
-        // create a bag
-        Bag smiths = model.createBag();
-        
-        // select all the resources with a VCARD.FN property
-        // whose value ends with "Smith"
-        StmtIterator iter = model.listStatements(new SimpleSelector(null, VCARD.FN, (RDFNode) null) {
-                    @Override
-                    public boolean selects(Statement s) {
-                            return s.getString().endsWith("Smith");
-                    }
-                });
-        // add the Smith's to the bag
-        while (iter.hasNext()) {
-            smiths.add( iter.nextStatement().getSubject());
-        }
-        
-        // print the graph as RDF/XML
-        model.write(new PrintWriter(System.out));
-        System.out.println();
-        
-        // print out the members of the bag
-        NodeIterator iter2 = smiths.iterator();
-        if (iter2.hasNext()) {
-            System.out.println("The bag contains:");
-            while (iter2.hasNext()) {
-                System.out.println("  " +
-                    ((Resource) iter2.next())
-                                     .getRequiredProperty(VCARD.FN)
-                                     .getString());
-            }
-        } else {
-            System.out.println("The bag is empty");
-        }
+		
+		// create the resource
+		Resource r = model.createResource();                                     
+		
+		// add the property
+		r.addProperty(RDFS.label, model.createLiteral("chat", "en"))
+		 .addProperty(RDFS.label, model.createLiteral("chat", "fr"))
+		 .addProperty(RDFS.label, model.createLiteral("<em>chat</em>", true));
+		  
+		// write out the graph
+		model.write(new PrintWriter(System.out));
+		System.out.println();
+		  
+		// create an empty graph
+		model = ModelFactory.createDefaultModel();
+		
+		// create the resource
+		r = model.createResource();                                     
+		
+		// add the property
+		r.addProperty(RDFS.label, "11").addLiteral(RDFS.label, 11);
+		
+		// write out the graph
+		model.write( System.out, "N-TRIPLE");
 	}
 }

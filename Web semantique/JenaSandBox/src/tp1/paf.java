eeb2a29c16900573836db135321a5cb354500749
E1drad <charles.eric44@gmail.com>
2016-10-04 16:20:34 +0200

    Update WebSem TP1 - Navigating a Model

--- Web semantique/JenaSandBox/src/tp1/paf.java
+++ Web semantique/JenaSandBox/src/tp1/paf.java
@@ -1,6 +1,7 @@
 package tp1;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
@@ -18,12 +19,40 @@ import org.apache.jena.vocabulary.VCARD;
 
 public class paf {
 	static final String inputFileName = "src/tp1/vc-db-1.rdf";
-	
+	static final String johnSmithURI = "http://somewhere/JohnSmith/";
+    
 	public static void main (String args[]) {
+		// create an empty model
+	    Model model = ModelFactory.createDefaultModel();
+	   
+	    // use the FileManager to find the input file
+		InputStream in = FileManager.get().open(inputFileName);
+		if (in == null) {
+		    throw new IllegalArgumentException( "File: " + inputFileName + " not found");
+		}
 		
-		Model m2 = ModelFactory.createDefaultModel();
-		m2.read("src/tp1/fragment.rdf");
-		m2.write(System.out);
+		// read the RDF/XML file
+		model.read(new InputStreamReader(in), "");
 		
+		// retrieve the Adam Smith vcard resource from the model
+		Resource vcard = model.getResource(johnSmithURI);
+		
+		// retrieve the value of the N property
+		Resource name = (Resource) vcard.getRequiredProperty(VCARD.N)
+		                                .getObject();
+		// retrieve the given name property
+		String fullName = vcard.getRequiredProperty(VCARD.FN)
+		                       .getString();
+		// add two nick name properties to vcard
+		vcard.addProperty(VCARD.NICKNAME, "Smithy")
+		 .addProperty(VCARD.NICKNAME, "Adman");
+		
+		// set up the output
+		System.out.println("The nicknames of \"" + fullName + "\" are:");
+		// list the nicknames
+		StmtIterator iter = vcard.listProperties(VCARD.NICKNAME);
+		while (iter.hasNext()) {
+			System.out.println("    " + iter.nextStatement().getObject().toString());
+		}
 	}
 }

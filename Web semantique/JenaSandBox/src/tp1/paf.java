721e2fdfdf8cd4a9309f9afd10ce166e0a557b31
E1drad <charles.eric44@gmail.com>
2016-10-04 16:54:59 +0200

    Update WebSem TP1 - Containers

--- Web semantique/JenaSandBox/src/tp1/paf.java
+++ Web semantique/JenaSandBox/src/tp1/paf.java
@@ -2,13 +2,17 @@ package tp1;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
+import org.apache.jena.rdf.model.Bag;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.NodeIterator;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.SimpleSelector;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.util.FileManager;
@@ -19,30 +23,52 @@ import org.apache.jena.vocabulary.VCARD;
  */
 
 public class paf {
-	static final String inputFileName1 = "src/tp1/vc-db-1.rdf";
-	static final String inputFileName2 = "src/tp1/vc-db-2.rdf";
+	static final String inputFileName = "src/tp1/vc-db-1.rdf";
     
 	public static void main (String args[]) {
 
-		Model model1 = ModelFactory.createDefaultModel();
-		Model model2 = ModelFactory.createDefaultModel();
-		InputStream in1 = FileManager.get().open(inputFileName2);		
-		if (in1 == null) {
-            throw new IllegalArgumentException( "File: " + inputFileName1 + " not found");
+		Model model = ModelFactory.createDefaultModel();
+		 // use the class loader to find the input file
+        InputStream in = FileManager.get().open( inputFileName );
+        if (in == null) {
+            throw new IllegalArgumentException( "File: " + inputFileName + " not found");
         }
-        InputStream in2 = FileManager.get().open(inputFileName2);
-        if (in2 == null) {
-            throw new IllegalArgumentException( "File: " + inputFileName2 + " not found");
+        
+        // read the RDF/XML file
+        model.read(new InputStreamReader(in), "");
+        
+        // create a bag
+        Bag smiths = model.createBag();
+        
+        // select all the resources with a VCARD.FN property
+        // whose value ends with "Smith"
+        StmtIterator iter = model.listStatements(new SimpleSelector(null, VCARD.FN, (RDFNode) null) {
+                    @Override
+                    public boolean selects(Statement s) {
+                            return s.getString().endsWith("Smith");
+                    }
+                });
+        // add the Smith's to the bag
+        while (iter.hasNext()) {
+            smiths.add( iter.nextStatement().getSubject());
         }
-		// lit les fichiers RDF/XML
-		model1.read(new InputStreamReader(in1), "");
-
-		model2.read(new InputStreamReader(in2), "");
-
-		// fusionne les modèles
-		Model model = model1.union(model2);
-
-		// affiche le modèle comme RDF/XML
-		model.write(System.out, "RDF/XML-ABBREV");
+        
+        // print the graph as RDF/XML
+        model.write(new PrintWriter(System.out));
+        System.out.println();
+        
+        // print out the members of the bag
+        NodeIterator iter2 = smiths.iterator();
+        if (iter2.hasNext()) {
+            System.out.println("The bag contains:");
+            while (iter2.hasNext()) {
+                System.out.println("  " +
+                    ((Resource) iter2.next())
+                                     .getRequiredProperty(VCARD.FN)
+                                     .getString());
+            }
+        } else {
+            System.out.println("The bag is empty");
+        }
 	}
 }
